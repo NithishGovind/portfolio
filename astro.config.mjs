ad8d7c1918da4624264b49b3745660398e76cc5b
NithishGovind <nithishg82@gmail.com>
2025-10-26 01:11:48 +0530

    final_commit_before_hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://nithishgovind.github.io',
+  base: '/portfolio',
   integrations: [mdx(), sitemap(), tailwind()]
 });
