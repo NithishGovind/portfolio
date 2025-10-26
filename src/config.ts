72d976b805742df3fbe54da9a28836763297f446
NithishGovind <nithishg82@gmail.com>
2025-10-26 10:57:59 +0530

    confirmed_desc

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
+export const SITE_TITLE = 'Nithish Govind | Robotics & AI Portfolio';
+export const SITE_DESCRIPTION = 'Portfolio of Nithish Govind: robotics, AI, embedded systems, automation, national achievements, and workshops.';
+export const GENERATE_SLUG_FROM_TITLE = true;
+export const TRANSITION_API = true;
