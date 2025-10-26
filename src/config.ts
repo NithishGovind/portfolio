5cec3a2fedeee50a77b0eb148b96a6cecc3fccbf
NithishGovind <nithishg82@gmail.com>
2025-10-26 11:02:05 +0530

    wrapped_desc

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Nithish Govind | Robotics & AI Portfolio';
-export const SITE_DESCRIPTION = 'Portfolio of Nithish Govind: robotics, AI, embedded systems, automation, national achievements, and workshops.';
+export const SITE_TITLE = 'Nithish | Portfolio Website';
+export const SITE_DESCRIPTION = '';
 export const GENERATE_SLUG_FROM_TITLE = true;
 export const TRANSITION_API = true;
